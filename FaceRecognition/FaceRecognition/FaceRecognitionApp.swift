f76de699b03d56eb17f2ac2562527ac02d69fe23
009Benny <009bennyreyes@gmail.com>
2023-02-25 12:10:47 -0600

    se agrega proyecto de iOS

---
+++ FaceRecognition/FaceRecognition/FaceRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceRecognitionApp.swift
+//  FaceRecognition
+//
+//  Created by Benny Reyes on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
